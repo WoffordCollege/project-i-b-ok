4e2a4b0e3d4250038e937542cd899b4b77b22d21
Marshall Wicker <mwicker575@gmail.com>
2019-10-21 15:02:41 -0400

    Set up console main properly

--- src/test/java/edu/wofford/wocoin/ConsoleMainTest.java
+++ src/test/java/edu/wofford/wocoin/ConsoleMainTest.java
@@ -1,4 +1,40 @@
 package edu.wofford.wocoin;
 
+import edu.wofford.wocoin.main.Main;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import static org.testng.Assert.assertEquals;
+
 public class ConsoleMainTest {
+
+    private String sendProgramInput(String input) {
+        String actualOutput = null;
+        InputStream originalIn = System.in;
+        PrintStream originalOut = System.out;
+
+        try {
+            System.setIn(new ByteArrayInputStream(input.getBytes()));
+            ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(outContent));
+            ConsoleMain.main(new String[0]);
+            actualOutput = outContent.toString();
+        } finally {
+            System.setIn(originalIn);
+            System.setOut(originalOut);
+        }
+
+        return actualOutput;
+    }
+
+    @Test
+    public void testLoginScreen() {
+        String output = sendProgramInput("");
+        assertEquals("1. exit\n2. administrator\n", output);
+    }
+
 }
