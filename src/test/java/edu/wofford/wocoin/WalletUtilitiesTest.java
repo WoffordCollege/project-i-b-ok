96a4c39b056943cdee226f33664238adce7fb410
Shahraiz Khan <khansa@email.wofford.edu>
2019-10-28 20:19:29 -0400

    Add WalletUtilities

---
+++ src/test/java/edu/wofford/wocoin/WalletUtilitiesTest.java
@@ -0,0 +1,26 @@
+package edu.wofford.wocoin;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.sql.*;
+
+public class WalletTest {
+    @Test
+    public final Void  publicPrivateKeyTest() {
+
+    }
+
+    @Test
+    public final Void subDirectoryCreatedTest() {
+
+    }
+
+    @Test
+    public final Void rootDirectoryTest() {
+
+    }
+    /*
+    test json creation?
+     */
+
+}
