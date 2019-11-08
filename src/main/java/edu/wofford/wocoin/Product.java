60497f2e40b59e16de0a3aa54279e3a98114a24b
Marshall Wicker <mwicker575@gmail.com>
2019-11-08 00:44:06 -0500

    Further updates to the Product class to enable changing of comparison and display types

--- src/main/java/edu/wofford/wocoin/Product.java
+++ src/main/java/edu/wofford/wocoin/Product.java
@@ -8,12 +8,31 @@ public class Product implements Comparable<Product>{
     private int price;
     private String name;
     private String description;
-    private String currentUser;
+    private String currentUserWallet;
     private boolean ownedByUser;
+    private CompareType compareType;
+    private DisplayType displayType;
+
+    /**
+     * This allows the user to choose the compare type for the products in {@link Product#compareTo(Product)}.
+     */
+    public enum CompareType {ALPHABETICALLY, PRICE}
+
+    /**
+     * This allows the user to choose a display type for the products using {@link Product#toString()}
+     */
+    public enum DisplayType {SHOWCURRENTUSER, HIDECURRENTUSER}
 
 
     public Product(String seller, int price, String name, String description) {
         this(seller, price, name, description, null);
+    }
+
+    /**
+     * Works with the main constructor to set the default values of the comparison type and display type
+     */
+    public Product(String seller, int price, String name, String description, String currentUserWallet) {
+        this(seller, price, name, description, currentUserWallet, CompareType.ALPHABETICALLY, DisplayType.HIDECURRENTUSER);
     }
 
     /**
@@ -22,18 +41,23 @@ public class Product implements Comparable<Product>{
      * @param price The price of the product
      * @param name The name of the product
      * @param description The description of the product
-     * @param currentUser The user of the UI using this product
+     * @param currentUserWallet The wallet address of the user using this product
+     * @param compareType Chooses what the {@link Product#compareTo(Product)} function uses to determine which object comes first
+     * @param displayType Chooses the method {@link Product#toString()} uses to show on display
      */
-    public Product(String seller, int price, String name, String description, String currentUser) {
+    public Product(String seller, int price, String name, String description, String currentUserWallet, CompareType compareType, DisplayType displayType) {
         this.seller = seller;
         this.price = price;
         this.name = name;
         this.description = description;
-        this.currentUser = currentUser;
+        this.currentUserWallet = currentUserWallet;
 
-        if (this.currentUser != null) {
-            this.ownedByUser = this.seller.equals(currentUser);
+        if (this.currentUserWallet != null) {
+            this.ownedByUser = this.seller.equals(currentUserWallet);
         }
+
+        this.compareType = compareType;
+        this.displayType = displayType;
     }
 
     public String getSeller() {
@@ -68,8 +92,8 @@ public class Product implements Comparable<Product>{
         this.description = description;
     }
 
-    public String getCurrentUser() {
-        return currentUser;
+    public String getCurrentUserWallet() {
+        return currentUserWallet;
     }
 
     /**
@@ -77,38 +101,59 @@ public class Product implements Comparable<Product>{
      * It is used in the toString method to determine if the right angle brackets should be visible.
      * If this remains unset, the toString method returns the standard description.
      * This also sets the ownedByUser flag to true if the currentUser equals the seller of the item.
-     * @param currentUser The user currently logged into the UI.
+     * @param currentUserWallet The user currently logged into the UI.
      */
-    public void setCurrentUser(String currentUser) {
-        this.currentUser = currentUser;
+    public void setCurrentUserWallet(String currentUserWallet) {
+        this.currentUserWallet = currentUserWallet;
 
-        if (currentUser != null){
-            this.ownedByUser = this.currentUser.equals(this.seller);
+        if (currentUserWallet != null){
+            this.ownedByUser = this.currentUserWallet.equals(this.seller);
         }
+    }
+
+    public CompareType getCompareType() {
+        return compareType;
+    }
+
+    public void setCompareType(CompareType compareType) {
+        this.compareType = compareType;
+    }
+
+    public DisplayType getDisplayType() {
+        return displayType;
+    }
+
+    public void setDisplayType(DisplayType displayType) {
+        this.displayType = displayType;
     }
 
     /**
      * Takes the information from the class to determine the string representation of the object.
-     * If currentUser is set, and the seller of the item is the same
-     * @return
+     * If currentUser is set, and the seller of the item is the same as the currentUser, and the displayType is SHOWCURRENTUSER,
+     * returns a string in the form <code>>>>  name: description  [n WoCoin(s)]</code>
+     * Otherwise returns a string in the form <code>name: description  [n WoCoin(s)]</code>
+     * @return a string representing the Product object.
      */
     @Override
     public String toString() {
-        String angleBracketsForCurrentUser = ownedByUser ? ">>>  " : "";
+        String angleBracketsForCurrentUser = displayType == DisplayType.SHOWCURRENTUSER && this.ownedByUser ? ">>>  " : "";
         String wocoinOrWocoins = this.price == 1 ? "WoCoin" : "WoCoins";
         return String.format("%s%s: %s  [%d %s]", angleBracketsForCurrentUser, this.name, this.description, this.price, wocoinOrWocoins);
     }
 
+
+
     /**
      * Compares two products based on their price, then their description.
-     * If the price of the two objects are different, returns a number &lt; 0 if this product has a lower price and a a number &gt; 0 if this product has a higher price.
-     * Otherwise it compares the two product names lexicographically using the String compareTo.
+     * If the price of the two objects are different, and the CompareType is price,
+     * returns a number &lt; 0 if this product has a lower price and a a number &gt; 0 if this product has a higher price.
+     * Otherwise it compares the two product names lexicographically using the {@link String#compareTo(String)}.
      * @param otherProduct the Product to be compared to
      * @return a negative integer, zero, or a positive integer as this product is less than, equal to, or greater than the specified product.
      */
     @Override
     public int compareTo(Product otherProduct) {
-        if (this.price != otherProduct.getPrice()) {
+        if (this.compareType == CompareType.PRICE && this.price != otherProduct.getPrice()) {
             return this.price - otherProduct.getPrice();
         }
         else {
--- src/test/java/edu/wofford/wocoin/ProductTest.java
+++ src/test/java/edu/wofford/wocoin/ProductTest.java
@@ -9,13 +9,15 @@ import java.util.ArrayList;
 import static org.junit.Assert.*;
 
 public class ProductTest {
-    Product skittles1;
-    Product chalk2;
-    Product zombieland3;
-    Product apple4;
-    Product paper5;
-    Product risk6;
-    Product charlotteTrip7;
+    private Product skittles1;
+    private Product chalk2;
+    private Product zombieland3;
+    private Product apple4;
+    private Product paper5;
+    private Product risk6;
+    private Product tripToCharlotte7;
+
+    private ArrayList<Product> products;
 
 
     @Before
@@ -37,7 +39,17 @@ public class ProductTest {
         apple4 = new Product("nottestuser", 3, "apple", "small");
         paper5 = new Product("nottestuser", 4, "paper", "a ream for a printer");
         risk6 = new Product("testuser", 4, "Risk", "board game");
-        charlotteTrip7 = new Product("nottestuser", 4, "trip to Charlotte", "no questions asked");
+        tripToCharlotte7 = new Product("nottestuser", 4, "trip to Charlotte", "no questions asked");
+
+        products = new ArrayList<>();
+
+        products.add(skittles1);
+        products.add(chalk2);
+        products.add(zombieland3);
+        products.add(apple4);
+        products.add(paper5);
+        products.add(risk6);
+        products.add(tripToCharlotte7);
     }
 
     @After
@@ -56,44 +68,80 @@ public class ProductTest {
           6: >>>  Risk: board game  [4 WoCoins]
           7: trip to Charlotte: no questions asked  [4 WoCoins]
          */
-        skittles1.setCurrentUser("testuser");
-        chalk2.setCurrentUser("testuser");
-        zombieland3.setCurrentUser("testuser");
-        apple4.setCurrentUser("testuser");
-        risk6.setCurrentUser("testuser");
+        skittles1.setCurrentUserWallet("testuser");
+        skittles1.setDisplayType(Product.DisplayType.SHOWCURRENTUSER);
+        chalk2.setCurrentUserWallet("testuser");
+        zombieland3.setCurrentUserWallet("testuser");
+        apple4.setCurrentUserWallet("testuser");
+        risk6.setCurrentUserWallet("testuser");
 
         assertEquals(">>>  skittles: a half-eaten bag  [1 WoCoin]", skittles1.toString());
         assertEquals("chalk: taken from a classroom  [2 WoCoins]", chalk2.toString());
-        assertEquals(">>>  Zombieland: DVD  [2 WoCoins]", zombieland3.toString());
+        assertEquals("Zombieland: DVD  [2 WoCoins]", zombieland3.toString());
         assertEquals("apple: small  [3 WoCoins]", apple4.toString());
         assertEquals("paper: a ream for a printer  [4 WoCoins]", paper5.toString());
-        assertEquals(">>>  Risk: board game  [4 WoCoins]", risk6.toString());
-        assertEquals("trip to Charlotte: no questions asked  [4 WoCoins]", charlotteTrip7.toString());
+        assertEquals("Risk: board game  [4 WoCoins]", risk6.toString());
+        assertEquals("trip to Charlotte: no questions asked  [4 WoCoins]", tripToCharlotte7.toString());
     }
 
     @Test
     public void compareTo() {
-        assertTrue(skittles1.compareTo(chalk2) < 0);
-        assertTrue(chalk2.compareTo(zombieland3) < 0);
-        assertTrue(zombieland3.compareTo(apple4) < 0);
-        assertTrue(apple4.compareTo(paper5) < 0);
+
+        // Test that the order of the items when comparing by price first is correct
+        products.get(0).setCompareType(Product.CompareType.PRICE);
+
+        for (int i = 0; i < products.size() - 1; i++) {
+            products.get(i + 1).setCompareType(Product.CompareType.PRICE);
+            assertTrue(products.get(i).compareTo(products.get(i + 1)) < 0);
+            assertEquals(0, products.get(i).compareTo(products.get(i)));
+            assertTrue(products.get(i + 1).compareTo(products.get(i)) > 0);
+        }
+
+        for (Product product : products) {
+            product.setCompareType(Product.CompareType.ALPHABETICALLY);
+            assertEquals(0, product.compareTo(product));
+        }
+
+        assertTrue(apple4.compareTo(chalk2) < 0);
+        assertTrue(chalk2.compareTo(paper5) < 0);
         assertTrue(paper5.compareTo(risk6) < 0);
-        assertTrue(risk6.compareTo(charlotteTrip7) < 0);
+        assertTrue(risk6.compareTo(skittles1) < 0);
+        assertTrue(skittles1.compareTo(tripToCharlotte7) < 0);
+        assertTrue(tripToCharlotte7.compareTo(zombieland3) < 0);
 
-        assertEquals(0, skittles1.compareTo(skittles1));
-        assertEquals(0, chalk2.compareTo(chalk2));
-        assertEquals(0, zombieland3.compareTo(zombieland3));
-        assertEquals(0, apple4.compareTo(apple4));
-        assertEquals(0, paper5.compareTo(paper5));
-        assertEquals(0, risk6.compareTo(risk6));
-        assertEquals(0, charlotteTrip7.compareTo(charlotteTrip7));
-
-
-        assertTrue(chalk2.compareTo(skittles1) > 0);
-        assertTrue(zombieland3.compareTo(chalk2) > 0);
-        assertTrue(apple4.compareTo(zombieland3) > 0);
-        assertTrue(paper5.compareTo(apple4) > 0);
+        assertTrue(zombieland3.compareTo(tripToCharlotte7) > 0);
+        assertTrue(tripToCharlotte7.compareTo(skittles1) > 0);
+        assertTrue(skittles1.compareTo(risk6) > 0);
         assertTrue(risk6.compareTo(paper5) > 0);
-        assertTrue(charlotteTrip7.compareTo(risk6) > 0);
+        assertTrue(paper5.compareTo(chalk2) > 0);
+        assertTrue(chalk2.compareTo(apple4) > 0);
+    }
+
+    @Test
+    public void testSortingListWorksCorrectly() {
+        for(Product product : products) {
+            product.setDisplayType(Product.DisplayType.SHOWCURRENTUSER);
+            product.setCompareType(Product.CompareType.PRICE);
+            product.setCurrentUserWallet("testuser");
+        }
+
+        products.sort(Product::compareTo);
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < products.size() - 1; i++){
+            sb.append(String.format("%d: %s\n", i + 1, products.get(i).toString()));
+        }
+        sb.append(String.format("%d: %s", products.size(), products.get(products.size() - 1)));
+
+        String expected = "1: >>>  skittles: a half-eaten bag  [1 WoCoin]\n" +
+                          "2: chalk: taken from a classroom  [2 WoCoins]\n" +
+                          "3: >>>  Zombieland: DVD  [2 WoCoins]\n" +
+                          "4: apple: small  [3 WoCoins]\n" +
+                          "5: paper: a ream for a printer  [4 WoCoins]\n" +
+                          "6: >>>  Risk: board game  [4 WoCoins]\n" +
+                          "7: trip to Charlotte: no questions asked  [4 WoCoins]";
+
+        assertEquals(expected, sb.toString());
     }
 }
