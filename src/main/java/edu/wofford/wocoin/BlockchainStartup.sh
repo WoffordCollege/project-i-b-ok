0d7eeaf78967c6dac04a4d8fa84f32917892160a
Marshall Wicker <mwicker575@gmail.com>
2019-11-21 13:30:57 -0500

    Change startup script to use relative paths. Need to make sure it is functional on windows.

--- src/main/java/edu/wofford/wocoin/BlockchainStartup.sh
+++ src/main/java/edu/wofford/wocoin/BlockchainStartup.sh
@@ -1,2 +1,4 @@
-geth --datadir ./node0 init ./genesis.json
-geth --rpc --rpcapi personal,eth,net,web3,miner --mine --miner.etherbase 0x0fce4741f3f54fbffb97837b4ddaa8f769ba0f91 --nodiscover --rpccorsdomain "*" --datadir ./node0 --miner.noverify --allow-insecure-unlock --unlock 0 --password C:\Users\cburd\project-i-b-ok\ethereum\node0\keystore\password.txt
+project_root=$(dirname $(dirname $(dirname $(dirname $(dirname $(dirname $(dirname $0 )))))))
+echo $project_root
+geth --datadir "$project_root/ethereum/node0" init "$project_root/ethereum/genesis.json"
+geth --rpc --rpcapi personal,eth,net,web3,miner --mine --miner.etherbase 0x0fce4741f3f54fbffb97837b4ddaa8f769ba0f91 --nodiscover --rpccorsdomain "*" --datadir "$project_root/ethereum/node0" --miner.noverify --allow-insecure-unlock --unlock 0 --password "$project_root/ethereum/node0/keystore/password.txt"
